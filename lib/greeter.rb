8ad3d04ce5ad767a35b6aa52f9033c2236324c33
Osuka-Andala <teresa.andala@gmail.com>
2015-08-21 08:23:53 +0300

    Added rack response

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,7 +1,8 @@
 class Greeter
 
  def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello Rack"]]
+    # [200, {"Content-Type" => "text/plain"}, ["Hello Rack"]]
+     Rack::Response.new("Hello")
  end
 
 end
