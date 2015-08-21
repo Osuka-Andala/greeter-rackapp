3028955d32d953859867ec8eaeb0a80a2de1dbd8
Osuka-Andala <teresa.andala@gmail.com>
2015-08-21 08:30:20 +0300

    Added erb template

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,8 +1,18 @@
+require "erb"
+
+
 class Greeter
 
  def call(env)
     # [200, {"Content-Type" => "text/plain"}, ["Hello Rack"]]
-     Rack::Response.new("Hello")
+     Rack::Response.new(render("index.html.erb"))
  end
 
+
+ def render(template)
+	path = File.expand_path("../views/#{template}", __File__)
+	ERB.new(File.read(path)).result(binding)
+	
+ end
+ 
 end
