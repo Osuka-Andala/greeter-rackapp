4b8ac23241ed88b70eb19c2c28ca7f3de46d05d0
Osuka-Andala <teresa.andala@gmail.com>
2015-08-21 08:54:55 +0300

    Added request

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -5,6 +5,7 @@ class Greeter
 
  def call(env)
     # [200, {"Content-Type" => "text/plain"}, ["Hello Rack"]]
+     request = Rack::Request.new(env)
      Rack::Response.new(render("index.html.erb"))
  end
 
